d3037d0d5dc75ed3a19940cb2901325383fffb48
bereketheramo25@gmail.com <bereketheramo25@gmail.com>
2024-06-01 05:52:13 +0300

    ui modification on discover page

--- lib/features/Discover/presentation/screens/discover_page.dart
+++ lib/features/Discover/presentation/screens/discover_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:shoesly/core/constants/app_text_styles.dart';
 import 'package:shoesly/core/constants/assets.dart';
+import 'package:shoesly/core/theme/colors.dart';
 import 'package:shoesly/core/widgets/custom_app_bar.dart';
 import 'package:shoesly/features/Discover/data/models/shoe.dart';
 import 'package:shoesly/features/Discover/presentation/widgets/shoe_card.dart';
@@ -17,6 +19,10 @@ class DiscoverPage extends StatefulWidget {
 class _DiscoverPageState extends State<DiscoverPage>
     with TickerProviderStateMixin {
   late TabController _tabController;
+  int _selectedIndex = 0;
+  final PageController _pageController = PageController();
+
+  final List<String> _tabs = ["All", "Nike", "Jordan", "Addidas", "Reebok","Vans"];
   @override
   void initState() {
     _tabController = TabController(length: 5, vsync: this, initialIndex: 0);
@@ -27,7 +33,8 @@ class _DiscoverPageState extends State<DiscoverPage>
     Shoe(
       imageUrl:
           'https://cdn.pixabay.com/photo/2016/03/05/19/02/hamburger-1238246_1280.jpg', // Replace with actual URLs
-      brandLogoUrl: 'https://cdn.pixabay.com/photo/2016/03/05/19/02/hamburger-1238246_1280.jpg',
+      brandLogoUrl:
+          'https://cdn.pixabay.com/photo/2016/03/05/19/02/hamburger-1238246_1280.jpg',
       shoeName: 'Jordan 1 Retro High Tie Dye',
       rating: 4.5,
       reviews: 1045,
@@ -37,7 +44,8 @@ class _DiscoverPageState extends State<DiscoverPage>
     Shoe(
       imageUrl:
           'https://cdn.pixabay.com/photo/2016/03/05/19/02/hamburger-1238246_1280.jpg', // Replace with actual URLs
-      brandLogoUrl: 'https://cdn.pixabay.com/photo/2016/03/05/19/02/hamburger-1238246_1280.jpg',
+      brandLogoUrl:
+          'https://cdn.pixabay.com/photo/2016/03/05/19/02/hamburger-1238246_1280.jpg',
       shoeName: 'Jordan 1 Retro High Tie Dye',
       rating: 4.5,
       reviews: 1045,
@@ -46,7 +54,8 @@ class _DiscoverPageState extends State<DiscoverPage>
     Shoe(
       imageUrl:
           'https://cdn.pixabay.com/photo/2016/03/05/19/02/hamburger-1238246_1280.jpg', // Replace with actual URLs
-      brandLogoUrl: 'https://cdn.pixabay.com/photo/2016/03/05/19/02/hamburger-1238246_1280.jpg',
+      brandLogoUrl:
+          'https://cdn.pixabay.com/photo/2016/03/05/19/02/hamburger-1238246_1280.jpg',
       shoeName: 'Jordan 1 Retro High Tie Dye',
       rating: 4.5,
       reviews: 1045,
@@ -56,7 +65,7 @@ class _DiscoverPageState extends State<DiscoverPage>
   @override
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
-     final screenHeight = MediaQuery.of(context).size.height;
+    final screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
         appBar: CustomAppBar(
           title: 'Discover',
@@ -68,119 +77,99 @@ class _DiscoverPageState extends State<DiscoverPage>
               ),
             )
           ],
-          bottom: TabBar(
-              isScrollable: true,
-              
-              indicatorPadding: EdgeInsets.zero,
-              controller: _tabController,
-              padding: const EdgeInsets.symmetric(horizontal: 15),
-              indicatorSize: TabBarIndicatorSize.tab,
-              tabs: const [
-                CustomTabBar(
-                  title: "All",
-                ),
-                CustomTabBar(
-                  title: "Nike",
-                ),
-                CustomTabBar(
-                  title: "Jordan",
-                ),
-                CustomTabBar(
-                  title: "Addidas",
-                ),
-                CustomTabBar(
-                  title: "Reebok",
-                ),
-              ]),
+          // bottom: TabBar(
+          //     isScrollable: true,
+
+          //     indicatorPadding: EdgeInsets.zero,
+          //     controller: _tabController,
+          //     padding: const EdgeInsets.symmetric(horizontal: 15),
+          //     indicatorSize: TabBarIndicatorSize.tab,
+          //     tabs: const [
+          //       CustomTabBar(
+          //         title: "All",
+          //       ),
+          //       CustomTabBar(
+          //         title: "Nike",
+          //       ),
+          //       CustomTabBar(
+          //         title: "Jordan",
+          //       ),
+          //       CustomTabBar(
+          //         title: "Addidas",
+          //       ),
+          //       CustomTabBar(
+          //         title: "Reebok",
+          //       ),
+          //     ]),
         ),
-        body: TabBarView(
-          controller: _tabController,
-          children: [
-           Padding(padding: EdgeInsets.symmetric(horizontal: screenWidth*0.02,vertical: screenHeight*0.01),
-           child:  GridView.builder(
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2, // Number of columns
-                childAspectRatio: 0.8, // Adjust as needed
-                mainAxisSpacing: 5,
-                crossAxisSpacing: 2,
+        body:
+            // TabBarView(
+            //   controller: _tabController,
+            //   children: [
+            Padding(
+          padding: EdgeInsets.symmetric(
+              horizontal: screenWidth * 0.02, vertical: screenHeight * 0.01),
+          child: Column(children: [
+            Container(
+              height: screenHeight * 0.05,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: _tabs.length,
+                itemBuilder: (context, index) {
+                  return GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          _selectedIndex = index;
+                        });
+                        _pageController.jumpToPage(index);
+                      },
+                      child: Container(
+                        padding:
+                            const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                       
+                        child: Center(
+                          child: Text(_tabs[index],
+                              style: titleTextStyle.copyWith(
+                                  color: _selectedIndex == index
+                                      ? Colors.black
+                                      : AppColors.tabColor)),
+                        ),
+                      ));
+                },
               ),
-              itemCount: shoes.length,
-              itemBuilder: (context, index) {
-                return ShoeCard(shoe: shoes[index]);
-              },
             ),
-           ),
-           Padding(padding: EdgeInsets.symmetric(horizontal: screenWidth*0.02,vertical: screenHeight*0.01),
-           child:  GridView.builder(
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2, // Number of columns
-                childAspectRatio: 0.8, // Adjust as needed
-                mainAxisSpacing: 5,
-                crossAxisSpacing: 2,
+            Expanded(
+              child: PageView.builder(
+                controller: _pageController,
+                onPageChanged: (index) {
+                  setState(() {
+                    _selectedIndex = index;
+                  });
+                },
+                itemCount: _tabs.length,
+                itemBuilder: (context, index) {
+                  return GridView.builder(
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2, // Number of columns
+                      childAspectRatio: 0.8, // Adjust as needed
+                      mainAxisSpacing: 5,
+                      crossAxisSpacing: 2,
+                    ),
+                    itemCount: shoes.length,
+                    itemBuilder: (context, index) {
+                      return ShoeCard(shoe: shoes[index]);
+                    },
+                  );
+                },
               ),
-              itemCount: shoes.length,
-              itemBuilder: (context, index) {
-                return ShoeCard(shoe: shoes[index]);
-              },
-            ),
-           ),
-           Padding(padding: EdgeInsets.symmetric(horizontal: screenWidth*0.02,vertical: screenHeight*0.01),
-           child:  GridView.builder(
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2, // Number of columns
-                childAspectRatio: 0.8, // Adjust as needed
-                mainAxisSpacing: 5,
-                crossAxisSpacing: 2,
-              ),
-              itemCount: shoes.length,
-              itemBuilder: (context, index) {
-                return ShoeCard(shoe: shoes[index]);
-              },
-            ),
-           ),
-           Padding(padding: EdgeInsets.symmetric(horizontal: screenWidth*0.02,vertical: screenHeight*0.01),
-           child:  GridView.builder(
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2, // Number of columns
-                childAspectRatio: 0.8, // Adjust as needed
-                mainAxisSpacing: 5,
-                crossAxisSpacing: 2,
-              ),
-              itemCount: shoes.length,
-              itemBuilder: (context, index) {
-                return ShoeCard(shoe: shoes[index]);
-              },
-            ),
-           ),
-           Padding(padding: EdgeInsets.symmetric(horizontal: screenWidth*0.02,vertical: screenHeight*0.01),
-           child:  GridView.builder(
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2, // Number of columns
-                childAspectRatio: 0.8, // Adjust as needed
-                mainAxisSpacing: 5,
-                crossAxisSpacing: 2,
-              ),
-              itemCount: shoes.length,
-              itemBuilder: (context, index) {
-                return ShoeCard(shoe: shoes[index]);
-              },
-            ),
-           ),
-           Padding(padding: EdgeInsets.symmetric(horizontal: screenWidth*0.02,vertical: screenHeight*0.01),
-           child:  GridView.builder(
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2, // Number of columns
-                childAspectRatio: 0.8, // Adjust as needed
-                mainAxisSpacing: 5,
-                crossAxisSpacing: 2,
-              ),
-              itemCount: shoes.length,
-              itemBuilder: (context, index) {
-                return ShoeCard(shoe: shoes[index]);
-              },
-            ),
-           )
-          ],
-        ));
+            )
+          ]),
+        )
+
+        //   ],
+        // )
+
+        );
   }
 }
